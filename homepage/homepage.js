40a3cbdd0cc67443aef48a82bae2169bf0419d7c
correialeo <leandroo774@outlook.com>
2024-05-05 17:41:03 -0300

    Frontend para celulares pequenos e médios feito e algumas alterações feitas no geral do projeto.

---
+++ homepage/homepage.js
@@ -0,0 +1,72 @@
+const nav = document.querySelector(".cab-nav");
+const btnMenu = document.querySelector(".btn-menu");
+const menu = document.querySelector(".menu");
+
+function handleButtonClick(event){
+    if (event.type === "touchstart") event.preventDefault();
+    event.stopPropagation();
+    nav.classList.toggle("active");
+    handleClickOutside(menu, () => {
+        nav.classList.remove("active");
+        setAria();
+    });
+    setAria();
+}
+
+function handleClickOutside(targetElement, callback){
+    const html = document.documentElement;
+    function handleHTMLClick(event){
+        if (!targetElement.contains(event.target)){
+            targetElement.removeAttribute("data-target");
+            html.removeEventListener("click", handleHTMLClick);
+            html.removeEventListener("touchstart", handleHTMLClick);
+            callback()
+        }
+    }
+    if (!targetElement.hasAttribute("data-target")){
+        html.addEventListener("click", handleHTMLClick);
+        html.addEventListener("touchstart", handleHTMLClick);
+        targetElement.setAttribute("data-target", "")
+    }
+}
+
+function setAria(){
+    const isActive = nav.classList.contains("active");
+    btnMenu.setAttribute("aria-expanded", isActive);
+    if (isActive) {
+        btnMenu.setAttribute("aria-label", "Fechar Menu");
+    } else {
+        btnMenu.setAttribute("aria-label", "Abrir Menu");
+    }
+}
+
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
+
+function ocultarElemento() {
+    const larguraTela = window.innerWidth;
+    const subTitle = document.querySelector('#subTitle-home-page')
+    const search = document.querySelector('#search')
+    const iphone = document.querySelector('#iphone')
+    const footer = document.querySelector('#foot-nav')
+    
+  
+    if (subTitle && search && iphone && footer) { 
+      if (larguraTela <= 768) {
+        subTitle.style.display = 'none';
+        search.style.display = 'none'
+        iphone.style.display = 'none'
+        footer.style.display = 'none'
+      } else {
+        subTitle.style.display = 'block';
+        search.style.display = 'block'
+        iphone.style.display = 'block'
+        footer.style.display = 'block'
+      }
+    }
+  }
+
+window.addEventListener('load', ocultarElemento);
+window.addEventListener('resize', ocultarElemento);
+
+
